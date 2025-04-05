e5ef95ee217f0190178bbd02aca51e59de789682
Huerte <huertejerald@gmail.com>
2025-04-05 11:26:17 +0800

    Added models for backend

--- src/store/models.py
+++ src/store/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    user_name = models.CharField(max_length=100, blank=True)
+    full_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    is_email_verified = models.BooleanField(default=False)
+
+    # Explicitly override the groups and user_permissions fields
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',  # Unique reverse accessor name for the custom user model
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_permissions_set',  # Unique reverse accessor name for the custom user model
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user_name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/', blank=True)
+
+class CartItem(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    items = models.ManyToManyField(CartItem)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
