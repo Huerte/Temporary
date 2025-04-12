6eaa07ac59d29cbe0887236db0d9fb268c334e62
Huerte <huertejerald@gmail.com>
2025-04-12 10:08:51 +0800

    improve header and add a category model for database

--- src/store/models.py
+++ src/store/models.py
@@ -5,9 +5,15 @@ from django.db import models
 from decimal import Decimal
 import uuid
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True)
