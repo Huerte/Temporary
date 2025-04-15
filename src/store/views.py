d0c10d46a26c46ab7e6866c5b5dbe1976ecf01ed
Huerte <huertejerald@gmail.com>
2025-04-15 17:52:27 +0800

    fix annoying add to cart and friends issues fixes

--- src/store/views.py
+++ src/store/views.py
@@ -304,7 +304,7 @@ def add_to_cart(request, product_id):
             cart_item.quantity += 1
             cart_item.save()
 
-    return redirect('cart-view')
+    return redirect(request.META.get('HTTP_REFERER', 'default-redirect-url'))
 
 @login_required(login_url='/login')
 def remove_from_cart(request, product_id):
