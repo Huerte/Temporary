9d8ae31dbd2d277da52bff76d00b58258e4d7186
claire jem dedicatoria <dedicatoria14@gmail.com>
2025-04-21 16:23:06 +0800

    navbar polished to some extent but lack functionalities

--- src/store/static/js/scripts.js
+++ src/store/static/js/scripts.js
@@ -1,3 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   // ===============================
   // Loading screen timeout fallback
