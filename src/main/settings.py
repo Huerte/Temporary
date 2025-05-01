120fd6ae5db90d3621915ba3346758659e854466
claire jem dedicatoria <dedicatoria14@gmail.com>
2025-05-01 18:12:49 +0800

    fixed duplicate static files

--- src/main/settings.py
+++ src/main/settings.py
@@ -151,7 +151,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'store', 'static'),
+    os.path.join(BASE_DIR, 'store', 'static'),  # Adjust this path if needed
 ]
 
 
